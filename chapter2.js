40db0f18764c938bd7f7d7b5b6d4557ff47ba0a2
gulistanboylu <gulistan.boylu@gmail.com>
2016-05-16 23:48:17 +0300

    Describing Interfaces with comment

---
+++ chapter2.js
@@ -0,0 +1,37 @@
+/**
+ * Created by merida on 13/05/16.
+ */
+/*
+
+ interface Composite {
+ funciton add(child);
+ funciton add(child);
+ funciton add(index);
+ }
+
+ interface FormItem {
+ funciton save();
+ }
+ */
+
+var CompositeForm = function (id, method, action) { //implements Composite, Formitem
+
+};
+
+//implement the Composite interface.
+
+CompositeForm.prototype.add = function (child) {
+
+};
+
+CompositeForm.prototype.remove = function (child) {
+};
+
+CompositeForm.prototype.getChild = function (index) {
+
+};
+
+// Implement the FormItem interface.
+
+CompositeForm.prototype.save = function () {
+};
